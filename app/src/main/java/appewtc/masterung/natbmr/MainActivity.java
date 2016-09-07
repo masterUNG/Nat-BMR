086305d92abc7cd35642baab6284dfd1a7debc01
masterUNG <phrombutr@gmail.com>
2016-09-07 14:28:29 +0700

    Load Image from Server Success

--- app/src/main/java/appewtc/masterung/natbmr/MainActivity.java
+++ app/src/main/java/appewtc/masterung/natbmr/MainActivity.java
@@ -4,13 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private static final String urlLogo = "http://swiftcodingthai.com/natt/ImageMaster/logo_bmi_master.png";
+    private EditText userEditText, passwordEditText;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        //Load Image From Server
+        Picasso.with(this).load(urlLogo).resize(180,180).into(imageView);
+
     }   // Main Method
 
     public void clickSignUpMain(View view) {
