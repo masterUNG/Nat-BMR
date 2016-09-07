47ff48c1cb8bcfca43023d22d0e9423243b56348
masterUNG <phrombutr@gmail.com>
2016-09-07 15:25:42 +0700

    Read JSON แว้ววว

--- app/src/main/java/appewtc/masterung/natbmr/MainActivity.java
+++ app/src/main/java/appewtc/masterung/natbmr/MainActivity.java
@@ -1,13 +1,19 @@
 package appewtc.masterung.natbmr;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,6 +40,51 @@ public class MainActivity extends AppCompatActivity {
 
     }   // Main Method
 
+    //Create Inner Class
+    private class SynUser extends AsyncTask<Void, Void, String> {
+
+        //Explicit
+        private Context context;
+        private String myUserString, myPasswordString;
+        private String[] loginStrings;
+        private static final String urlJSON = "http://swiftcodingthai.com/natt/get_user_master.php";
+
+        public SynUser(Context context, String myUserString, String myPasswordString) {
+            this.context = context;
+            this.myUserString = myUserString;
+            this.myPasswordString = myPasswordString;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("NattV2", "e doInBack ==> " + e.toString());
+                return null;
+            }
+
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("NattV2", "JSON ==> " + s);
+
+        }   // onPost
+
+    }   // SynUser Class
+
+
     public void clickWebSite(View view) {
 
         Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -56,7 +107,8 @@ public class MainActivity extends AppCompatActivity {
                     "Please Fill All Every Blank");
         } else {
             //No Space
-
+            SynUser synUser = new SynUser(this, userString, passwordString);
+            synUser.execute();
         }
 
     }   // clickSignIn
