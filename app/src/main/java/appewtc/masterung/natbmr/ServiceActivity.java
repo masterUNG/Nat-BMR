d1e005ffdb25441a05c9c5ecb8cabd68c1fbc7d5
masterUNG <phrombutr@gmail.com>
2016-09-07 16:56:40 +0700

    Finish 7Sep

--- app/src/main/java/appewtc/masterung/natbmr/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/natbmr/ServiceActivity.java
@@ -7,11 +7,22 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class ServiceActivity extends AppCompatActivity {
 
     private String[] loginStrings;
     private EditText weightEditText, heightEditText, ageEditText;
     private TextView textView;
+    private static final String urlPHP = "http://swiftcodingthai.com/natt/edit_user_master.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,30 @@ public class ServiceActivity extends AppCompatActivity {
     }   // Main Method
 
     public void clickUpdateBMI(View view) {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Weight", weightEditText.getText().toString().trim())
+                .add("Height", heightEditText.getText().toString().trim())
+                .add("Age", ageEditText.getText().toString().trim())
+                .add("BMI", textView.getText().toString().trim())
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                Log.d("NattV4", "Result ==> " + response.body().string());
+            }
+        });
+
 
     }   // clickUpdate
 
